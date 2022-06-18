a9c279b25bd008b3ebb727d9ecae0ff6f6cbd38b
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 23:41:50 +0200

    i am white and young

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import cv2
+from cv2 import COLOR_BGR2GRAY
 
 layout = [
     [sg.Image(key = '-IMAGE-')],
@@ -7,9 +9,18 @@ layout = [
 
 window = sg.Window('Face Detector', layout)
 
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 while True:
-    event,values = window.read()
+    event,values = window.read(timeout = 0)
     if event == sg.WIN_CLOSED:
         break
+
+    _, frame = video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    imgbytes = cv2.imencode('.png',gray)[1].tobytes()
+    window['-IMAGE-'].update(data = imgbytes)
 
 window.close()
